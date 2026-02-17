e9a83ec4ce93d93f990caf03565fae3757c90c7e
Rasmus Thunberg <rath701@gmail.com>
2026-02-17 20:59:05 +0100

    Added capacitator

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fika.app',
+  appName: 'Fika',
+  webDir: 'dist'
+};
+
+export default config;
